eb4e43cadea2b0fcf83adceb45ca00adbd2458d0
Murillo de Morais <murillo.morais@vianuvem.com.br>
2022-02-15 22:00:48 -0300

    feat: adds images

--- src/components/WelcomeImages/index.js
+++ src/components/WelcomeImages/index.js
@@ -1,10 +1,9 @@
-const WelcomeImages = () => {
-
-  console.log('WelcomeImages')
-
-  return (
-    <div>Images</div>
-  );
-}
+const WelcomeImages = () => (
+  <div>
+    <img src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />
+    <img src="https://images.unsplash.com/photo-1496171367470-9ed9a91ea931?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />   
+    <img src="https://images.unsplash.com/photo-1563986768494-4dee2763ff3f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />   
+  </div>
+);
 
 export default WelcomeImages;
