84113f8bde369298fbda5ec35e0110881c961b6c
Murillo de Morais <murillo.morais@vianuvem.com.br>
2022-02-17 19:32:16 -0300

    feat: implements images "carousel"

--- src/components/WelcomeImages/index.js
+++ src/components/WelcomeImages/index.js
@@ -1,11 +1,23 @@
-import { Image } from './styles'
+import { Container, Image } from './styles'
+
+const images = [
+  "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  "https://images.unsplash.com/photo-1496171367470-9ed9a91ea931?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  "https://images.unsplash.com/photo-1563986768494-4dee2763ff3f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+]
 
 const WelcomeImages = () => (
-  <div>
-    <Image alt="imagem 1" src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />
-    <Image alt="imagem 2" src="https://images.unsplash.com/photo-1496171367470-9ed9a91ea931?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />   
-    <Image alt="imagem 3" src="https://images.unsplash.com/photo-1563986768494-4dee2763ff3f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />   
-  </div>
+  <Container>
+    {
+      images.map((image, i) => 
+        <Image
+          key={i}
+          i={i}
+          alt={`imagem ${i}`}
+          src={image} />    
+      )
+    }
+  </Container>
 );
 
 export default WelcomeImages;
--- src/components/WelcomeImages/styles.js
+++ src/components/WelcomeImages/styles.js
@@ -1,5 +1,17 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+  position: relative;
+`
+
 export const Image = styled.img`
-  width: 70%;
+  border-radius: 5px;
+  box-shadow: -3px 3px 30px rgba(0,0,0,.35);
+  position: absolute;
+  width: 25vw;
+
+  ${({ i }) => `
+    left: ${30 * i}px;
+    top: ${30 * i}px;
+  `}
 `
